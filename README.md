6ebf310c003792c2ae3009f27f678e5086786756
ARodriguezJara <rodrigueza19@uabc.edu.mx>
2022-05-17 19:33:50 -0700

    Formulario

---
+++ README.md
@@ -0,0 +1,13 @@
+# Proyecto020
+En este ejemplo de materials, realizamos un formulario simple, en este caso fue la suma de 2 numeros
+primero lo que hicimos importar los componentes de angular materials en 
+app.module.ts, utilizamos los componentes de
+"FormsModule", "MatInput" y "MatButton"
+que hacemos con estos?
+llamamos a los componentes de formulario, input y boton de angular
+despues dentro del archivo app-component.ts
+creamos un metodo donde realizamos la suma ingresada en el navegador,
+Dentro del archivo html
+utilizamos los contenedores <mat-form-field>
+para llamar los componentes de angular materials en el archivo html
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
